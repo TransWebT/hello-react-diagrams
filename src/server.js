1a4113dc7c20335c1ac79fbc284fea40ec5c60cc
Terry Tompkins <tompkins@transwebtools.com>
2021-02-24 22:47:16 -0500

    working on function organization for table interrogation.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var mysql = require('mysql');
 
+/*
+var connectInfo = {
+  host: "localhost",
+  user: "root",
+  password: "letmein4",
+  database: "world"
+}
+*/
+
 var connectInfo = {
   host: "localhost",
   user: "",
-  password: "",
-  database: "world"
+  password: ""
 }
 
-function connectToDatabase(connectInfo) {
+
+function getDatabaseConnection(connectInfo) {
   let connection = mysql.createConnection(connectInfo);
   connection.connect(function (err) {
     if (err) {
@@ -21,43 +30,54 @@ function connectToDatabase(connectInfo) {
   return connection;
 }
 
-function getTableNames(connection) {
+function getTableNames(connection, database) { 
   let sql = `select table_schema as database_name, table_name
              from information_schema.tables
              where table_type = 'BASE TABLE'
                    and table_schema not in ('information_schema','mysql','performance_schema','sys')
             order by database_name, table_name;`
-  tables = connection.query(sql, function(error, results, fields) {
+ /*
+  let sql = `select table_schema as database_name, table_name
+             from information_schema.tables
+             where table_type = 'BASE TABLE'
+                   and table_schema = '${database}'
+             order by database_name, table_name;`
+  */
+    let tables = connection.query(sql, function(error, results, fields) {
     if (error) {
       console.log('error querying: ' + error.stack);
       throw error;
     }
-    // console.log(results);
+    console.log(results);
     return results;
   });
-  console.log(tables);
+  // console.log(tables);
   return tables;
 }
 
-function readTables(database) {
-  let sql = "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'world' AND TABLE_NAME = 'city'";
+function getSchemaForTable(connection, database, table) {
+  let sql = `SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '${database}' AND TABLE_NAME = '${table}';`;
   connection.query(sql, function(error, results, fields) {
       if (error) {
         console.log('error querying: ' + error.stack);
         throw error;
       }
-      console.log(results);
+      // console.log(results);
+      return results;
   });
+  return results;
 }
 
-readTables("world");
+let connection = getDatabaseConnection(connectInfo);
+let tables = getTableNames(connection, "sparcs_tdm");
+console.log(tables);
+
 connection.end(function(err) {
   // The connection is terminated now
 });
 
-
+/*
 var app = express();
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -65,4 +85,4 @@ app.get('/', function (req, res) {
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
-
+*/
